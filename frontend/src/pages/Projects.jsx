d79dadb2c299c3a6a4a79c9ff2ba967c6a956b66
Cofefefe <mathieu.baugard@g-mail.fr>
2023-01-10 16:48:27 +0100

    feat:projects page 50%, find logic, need to rewrite properly

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -4,64 +4,101 @@ import Nav from '../components/Nav';
 import { AiFillHtml5,AiFillGithub} from 'react-icons/ai';
 import { SiCss3, SiJavascript, SiReact, SiSass, SiMongodb, SiBootstrap } from 'react-icons/si';
 
-let aside1 = document.getElementsByClassName('test1')
-let image1 = document.getElementById('image1')
 
-console.log({aside1}+{image1})
+import booki from '../assets/projects/bookivisual.png'
+import kanap from '../assets/projects/Kanap.PNG'
+import omf from '../assets/projects/Ohmyfood.PNG'
+import panthere from '../assets/projects/Lapanthère.PNG'
 
-
-function showImage(){
-        image1.style.display='block'
+function Project({ name, image, onMouseEnter, onMouseLeave }) {
+  return (
+    <div
+      className="project"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <img src={image} alt={name} />
+      <p>{name}</p>
+    </div>
+  );
 }
 
 
-function Projects() {
-    return(
-        <>
-            <Nav />
-            <main className="projects d-flex col-12 w-100 h-100r">
-                <section className="projects__choice col-6 bg-light">
-                    <h5 className="text-center mt-5 mb-5">Selectionnez un projet, et parcourez le sur mon Githbub.</h5>
-                    <div className="projects__list d-flex justify-content-center flex-column align-items-center">
-                        <aside id="aside1" onClick={showImage} className="projects__list--card test1">Booki - Intégration d'une maquette - HTML / CSS</aside>
-                        <aside id="aside2" className="projects__list--card">OhMyFood - Animation - CSS</aside>
-                        <aside id="aside3" className="projects__list--card">La Panthère - Audit & optimisation SEO - HTML / CSS</aside>
-                        <aside id="aside4" className="projects__list--card">Kanap - Site e-commerce dynamique - HTML / CSS / JS</aside>
-                        <aside id="aside5" className="projects__list--card">Piiquante - Backend ( inscription, post ) - Node / JS</aside>
-                        <aside id="aside6" className="projects__list--card">Groupomania - Reseau social d'entreprise - React / Node</aside>
-                    </div>
-                <div className="projects__tools d-flex">
-                    <h5 className="mt-5 mb-4 projects__tools--title col-12">Outils maîtrisés</h5>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">HTML <AiFillHtml5 className="icon projects__tools--card--html"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">CSS<SiCss3 className="icon projects__tools--card--css"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">JS<SiJavascript className="icon projects__tools--card--js"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">React<SiReact className="icon projects__tools--card--react"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">SASS<SiSass className="icon projects__tools--card--sass"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">Bootstrap<SiBootstrap className="icon projects__tools--card--bt"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">Github<AiFillGithub className="icon projects__tools--card--git"/></div>
-                        <div className="projects__tools--card d-flex justify-content-between align-items-center">MongoDb<SiMongodb className="icon projects__tools--card--mongo"/></div>
-                </div>
-                </section>
-                <section className="project__display col-6 bg-warning w-50 h-100">
-                    {/* <img id="image1" data-image-url="" style={{display:'none'}}/>
-                    <img id="image2" data-image-url="" style={{display:'none'}}/>
-                    <img id="image3" data-image-url="" style={{display:'none'}}/>
-                    <img id="image4" data-image-url="" style={{display:'none'}}/>
-                    <img id="image5" data-image-url="" style={{display:'none'}}/>
-                    <img id="image6" data-image-url="" style={{display:'none'}}/> */}
-                    <div id="image1" data-image-url="" style={{display:'none', backgroundColor:'red'}}/>
-                    <div id="image2" data-image-url="" style={{display:'none'}}/>
-                    <div id="image3" data-image-url="" style={{display:'none'}}/>
-                    <div id="image4" data-image-url="" style={{display:'none'}}/>
-                    <div id="image5" data-image-url="" style={{display:'none'}}/>
-                    <div id="image6" data-image-url="" style={{display:'none'}}/>
+function Tools({name, image}){
 
-                </section>
-            </main>
-            
-        </>
-    )
-    
+  return(
+    <>
+    <div className="projects__tools--card d-flex justify-content-between align-items-center">
+        {name + image}
+    </div>
+    </>
+  )
 }
-
-export default Projects;
+function Projects(name, image){
+  const [selectedProject, setSelectedProject] = useState(null)
+  const projects = [
+    {name: "Booki - Intégration d'une maquette - HTML / CSS", image:booki },
+    {name: "OhMyFood - Animation - CSS", image:omf },
+    {name: "La Panthère - Audit & optimisation SEO - HTML / CSS", image:panthere },
+    {name: "Kanap - Site e-commerce dynamique - HTML / CSS / JS", image:kanap },
+    {name: "Piiquante - Backend ( inscription, post ) - Node / JS", image: "image1.jpg" },
+    {name: "Groupomania - Reseau social d'entreprise - React / Node", image: "image1.jpg" }
+  ]
+  const tools = [
+    {name: "HTML", image:<AiFillHtml5 className="projects__tools--card--html"/> },
+    {name: "CSS", image:<SiCss3 className="projects__tools--card--css"/>},
+    {name: "Bootstrap", image:<SiBootstrap className="projects__tools--card--bt"/> },
+    {name: "Javascript", image: <SiJavascript className="projects__tools--card--js"/> },
+    {name: "React", image: <SiReact className="projects__tools--card--react"/> },
+    {name: "SASS", image: <SiSass className="projects__tools--card--sass"/> },
+    {name: "Github", image: <AiFillGithub className="projects__tools--card--git"/> },
+    {name: "MongoDB", image: <SiMongodb className="projects__tools--card--mongo"/> }
+  ]
+  return (
+    <>
+      <Nav />
+      <main className="projects d-flex col-12 w-100 h-100 flex-row">
+        <section className="projects__choice col-6 bg-light">
+          <div className="projects__list d-flex justify-content-center flex-column align-items-center">
+            <h4 className="text-center mt-5 mb-5 col-6">
+              Selectionnez un projet, et parcourez le sur github
+            </h4>
+            {projects.map((project) => (
+              <Project
+                name={project.name}
+                onMouseEnter={() => setSelectedProject(project.image)}
+                onMouseLeave={() => setSelectedProject(null)}
+              />
+            ))}
+            </div>
+          <div className="projects__tools d-flex">
+            <h5 className="mt-5 mb-4 projects__tools--title col-12">
+              Outils maîtrisés
+            </h5>
+            {tools.map((tool) => (
+              <Tools name={tool.name} image={tool.image} />
+            ))}
+          </div>
+        </section>
+        <section className="project__display col-6 bg-warning w-50 h-100">
+        <div
+              style={{
+                flex: 1,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            />
+              {selectedProject && (
+                <img
+                  src={selectedProject}
+                  alt="project"
+                  className="project-image"
+                />
+              )}
+        </section>
+      </main>
+    </>
+  );
+}
+export default Projects
