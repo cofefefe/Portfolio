3f5536844dbb420e097f3b97418f674a8ac1a9db
Cofefefe <mathieu.baugard@g-mail.fr>
2023-01-24 10:53:51 +0100

    fix

--- frontend/src/components/Contactform.jsx
+++ frontend/src/components/Contactform.jsx
@@ -3,21 +3,25 @@ import react from 'react'
 function ContactForm(){
     return(
         <div className="sendmail">
-            <form name="sendmail" method="post" data-netlify="true">
-                <input type="hidden" name="form-name" value="sendmail" />
-                <p>
-                    <label for="name">Your Name: <input type="text" name="name"/></label>
-                </p>
-                <p>
-                    <label for="email">Your Email: <input type="email" name="email"/></label>
-                </p>
-                <p>
-                    <label for="message">Message: <textarea name="message"></textarea></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
-            </form>
+            <form
+        name="contact"
+        method="post"
+        className="contact__form"
+        data-netlify="true"
+        onSubmit="submit"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <label for="name">Nom: </label>
+        <input type="text" name="name" />
+
+        <label for="email">Email: </label>
+        <input type="email" name="email" />
+
+        <label for="message">Message: </label>
+        <textarea name="message"></textarea>
+
+        <button type="submit">Envoyer</button>
+      </form>
         </div>
     )
 }
